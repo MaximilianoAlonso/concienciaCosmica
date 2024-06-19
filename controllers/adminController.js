cb83a6c8cff951a60477955b1a3645da444178b8
MaximilianoAlonso <elgrungemaxi1986@gmail.com>
2024-06-19 12:22:53 -0300

    Creacion de dashboard

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,44 @@
 module.exports = {
-    dashboard: (req,res) => {
-        res.render("admin/profile")
-    }
+  dashboard: (req, res) => {
+    res.render("admin/dashboard");
+  },
+  loginAdmin: (req, res) => {
+    res.render("admin/login");
+  },
 
-}
+  /*------------- FORMS ----------------*/
+
+  /* USER */
+  formUser: (req, res) => {
+    res.render("admin/forms/users");
+  },
+  createUser: (req, res) => {},
+
+  /* CURSES */
+
+  formCurse: (req, res) => {
+    res.render("admin/forms/curses");
+  },
+  createCurse: (req, res) => {},
+
+  /* PRODUCTS */
+
+  formProduct: (req, res) => {
+    res.render("admin/forms/products");
+  },
+  createProduct: (req, res) => {},
+
+  /* ARTICLES */
+
+  formArticle: (req, res) => {
+    res.render("admin/forms/articles");
+  },
+  createArticle: (req, res) => {},
+
+  /* RESURCES */
+
+  formResource: (req, res) => {
+    res.render("admin/forms/resources");
+  },
+  createResource: (req, res) => {},
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,45 @@
-const express = require('express');
-const { dashboard } = require('../controllers/adminController');
+const express = require("express");
+const {
+  dashboard,
+  createArticle,
+  createCurse,
+  createProduct,
+  createResource,
+  createUser,
+  formArticle,
+  formCurse,
+  formProduct,
+  formResource,
+  formUser,
+  loginAdmin
+} = require("../controllers/adminController");
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/dashboard', dashboard )
-        
+   /* ----------------DASHBOARD--------------- */
+   router
+  .get("/", dashboard)
+  .get("/login", loginAdmin)
+
+
+  /*------------------ FORMS ------------------*/
+  /* user */
+  .get("/createUser", formUser)
+  .post("/createUser", createUser)
+
+  /* curses */
+  .get("/createCurse", formCurse)
+  .post("/createCurse", createCurse)
+
+  /* articles */
+  .get("/createArticle", formArticle)
+  .post("/createArticle", createArticle)
+
+  /* resources */
+  .get("/createResource", formResource)
+  .post("/createResource", createResource)
+
+  /* products */
+  .get("/createProduct", formProduct)
+  .post("/createProduct", createProduct);
+
 module.exports = router;
